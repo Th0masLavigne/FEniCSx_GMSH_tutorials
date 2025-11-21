6ea936e4692132d8bfb35fcc754fc6e0f246007f
Th0masLavigne <lavignethomas@hotmail.fr>
2025-11-21 11:01:14 +0100

    automate deployment

--- conf.py
+++ conf.py
@@ -1,2 +1,12 @@
+# conf.py
+
+# 1. Base URL and Sitemap Scheme (Already correct)
 html_baseurl = 'https://th0maslavigne.github.io/FEniCSx_GMSH_tutorials/'
 sitemap_url_scheme = "{link}"
+
+# 2. **CRITICAL FIX**: Re-declare extensions list, including sitemap
+# Note: You must include the standard extensions used by Jupyter Book/Sphinx Book Theme
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx_sitemap'  # <-- The required extension
+]
